96a2371517c93b848ba2c1ae5e98e01a0e2b3626
Travis Wyatt <travis.i.wyatt@gmail.com>
2024-10-06 00:14:18 -0700

    Pinch zoom

--- app/src/commonMain/kotlin/features/dictate/DictateScreen.kt
+++ app/src/commonMain/kotlin/features/dictate/DictateScreen.kt
@@ -3,10 +3,10 @@ package com.traviswyatt.qd.features.dictate
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,14 +17,13 @@ import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -47,12 +46,20 @@ class DictateScreen : Screen {
         AppTheme {
             Box {
                 val border = if (isDictating) Color.Green else Color.Transparent
+
+                val fontSize = remember { mutableStateOf(96f) }
+                val lineHeight = derivedStateOf { (fontSize.value * 1.1f).toInt() }
+                val transformState = rememberTransformableState { zoomChange, panChange, rotationChange ->
+                    fontSize.value *= zoomChange
+                }
+
                 Box(
                     Modifier
                         .background(color = MaterialTheme.colors.background)
                         .border(15.dp, border)
                         .padding(15.dp)
                         .fillMaxSize()
+                        .transformable(transformState)
                         .clickable {
                             screenModel.dictation.toggle()
                         }
@@ -62,7 +69,7 @@ class DictateScreen : Screen {
                     } else {
                         "⚠️ Dictation unavailable."
                     }
-                    Text(message, fontSize = 96.sp, lineHeight = 104.sp, fontWeight = FontWeight.Bold)
+                    Text(message, fontSize = fontSize.value.toInt().sp, lineHeight = lineHeight.value.sp, fontWeight = FontWeight.Bold)
                 }
 
                 val ipAddress = remember { wifiIpAddress() }
