43230fdc1926b848c5410c1d7d836a39cac52044
Travis Wyatt <travis.i.wyatt@gmail.com>
2024-10-10 09:28:38 -0700

    Clear transcript on host after 1 minute idle

--- app/src/commonMain/kotlin/Init.kt
+++ app/src/commonMain/kotlin/Init.kt
@@ -1,11 +1,18 @@
 package com.traviswyatt.qd
 
+import com.juul.khronicle.Log
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.SharingStarted.Companion.Eagerly
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
@@ -24,26 +31,42 @@ val client = settings.host
     }.stateIn(GlobalScope, Eagerly, null)
 
 fun init() {
-    GlobalScope.launch {
-        if (settings.getTransmit()) {
-            val host = settings.getHost()
-            if (host == null || !isReachable(host)) {
+    GlobalScope.configureHost()
+    GlobalScope.clearTranscriptOnIdle()
+}
+
+private fun CoroutineScope.configureHost() = launch {
+    if (settings.getTransmit()) {
+        val host = settings.getHost()
+        if (host == null || !isReachable(host)) {
+            launch {
+                val job = currentCoroutineContext().job
+
                 launch {
-                    val job = currentCoroutineContext().job
+                    settings.transmit.filterNot { it }.first()
+                    job.cancel()
+                }
 
-                    launch {
-                        settings.transmit.filterNot { it }.first()
-                        job.cancel()
+                launch {
+                    hostFinder.find()?.let { ip ->
+                        settings.setHost(ip)
                     }
-
-                    launch {
-                        hostFinder.find()?.let { ip ->
-                            settings.setHost(ip)
-                        }
-                        job.cancel()
-                    }
+                    job.cancel()
                 }
             }
         }
     }
+}
+
+private fun CoroutineScope.clearTranscriptOnIdle() {
+    transcript
+        .filterNot { it.isNullOrEmpty() }
+        .debounce(2.minutes)
+        .onEach {
+            if (!settings.getTransmit()) {
+                Log.debug { "Clearing transcript" }
+                transcript.value = ""
+            }
+        }
+        .launchIn(this)
 }
